ad402e05bf96d500fd6d5eb39aaf11ebc7a65ded
Neur0Devil <leshasgusch@gmail.com>
2026-02-14 23:32:42 +0300

    init

---
+++ main.py
@@ -0,0 +1,74 @@
+import asyncio
+import websockets
+import queue
+from json import dumps, loads
+HOST_REL_DICT = {}
+HOST_DICT = {}
+CLIENT_REL_DICT = {}
+CLIENT_DICT = {}
+MESSAGE_QUEUE = queue.Queue()
+def host_reg(websocket,info=None):
+    print("host_reg")
+    host_id = hash(websocket)
+    res = HOST_REL_DICT.get(host_id,None)
+    if not res:
+        HOST_DICT[host_id] = {"websocket":websocket,**info}
+        HOST_REL_DICT[host_id]=dict()
+        return True
+    return False
+def client_reg(websocket, host_id=None):
+    print("client_reg")
+    res = HOST_REL_DICT.get(host_id,None)
+    if res:
+        client_id = hash(websocket)
+        HOST_REL_DICT[host_id][client_id] = websocket
+        CLIENT_REL_DICT[client_id] = host_id
+        CLIENT_DICT[client_id] = {"websocket":websocket,}
+        return True
+    return False
+def send(websocket, message=None):
+    print("send")
+    user_id = hash(websocket)
+    res = next(item for item in (HOST_REL_DICT.get(user_id,False),CLIENT_REL_DICT.get(user_id,False)) if item is not None)
+    if res:
+        for subscriber in res:
+            sub = next(item for item in (HOST_DICT.get(subscriber,False),CLIENT_DICT.get(subscriber,False)) if item is not None)
+            MESSAGE_QUEUE.put_nowait(sub.websocket.send(dumps({"type":"message","message":message})))
+        return True
+    return False
+def get_hosts(websocket,_=None):
+    print("get_host")
+    MESSAGE_QUEUE.put_nowait(websocket.send(dumps(HOST_DICT)))
+async def handle_messages():
+    while True:
+        await MESSAGE_QUEUE.get()
+        print("sent message")
+async def handle_connection(websocket):
+    try:
+        async for message in websocket:
+            message = loads(message)
+            if not COMMANDS[message["type"]](message["message"]):
+                await websocket.send(dumps({"type":"fuck_off","message":"wtf do you want"}))
+    finally:
+        del_id = hash(websocket)
+        if res:=CLIENT_DICT.get(del_id,None):HOST_REL_DICT[res].pop(del_id,None)
+        if res:=HOST_DICT.get(del_id,None):
+            for client,server in CLIENT_REL_DICT.items():
+                if server==del_id:CLIENT_REL_DICT.pop(client)
+        for dict_for_del in (HOST_DICT,HOST_REL_DICT,CLIENT_DICT,CLIENT_REL_DICT):
+            dict_for_del.pop(del_id,None)
+        await websocket.close()
+
+COMMANDS = {
+    "HOST_REG":host_reg,
+    "CLI_REG":client_reg,
+    "SEND":send,
+    "GET_HOSTS":get_hosts
+    }
+async def main():
+    async with websockets.serve(handle_connection, "0.0.0.0", 8766):
+        await asyncio.Future()
+    await handle_messages()
+
+if __name__ == "__main__":
+    asyncio.run(main())
